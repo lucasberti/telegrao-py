6a1940deb9764ffbce0c20960f8e4495885d26c7
Lucas Berti <berti.lucas@gmail.com>
2017-12-15 13:20:52 -0200

    Atualizando caminho do arquivo de log do /xet

--- plugins/markov.py
+++ plugins/markov.py
@@ -2,7 +2,7 @@ from api import send_message
 import markovify
 
 def on_msg_received(msg, matches):
-    with open("/root/telegram-bot/wordcloud.txt") as f:
+    with open("/root/paitons/telegrao-py/data/log.txt") as f:
         model = markovify.NewlineText(f.read())
 
     text = model.make_sentence(tries=25)
