b90b810411138c7297a7fb9dffdd69e60bc7336b
Lucas Berti <berti.lucas@gmail.com>
2017-12-18 16:12:54 -0200

    Adicionando inspiração ao /xet

--- plugins/markov.py
+++ plugins/markov.py
@@ -2,10 +2,23 @@ from api import send_message
 import markovify
 
 def on_msg_received(msg, matches):
+    
+    chat = msg["chat"]["id"]
+
     with open("/root/paitons/telegrao-py/data/log.txt") as f:
         model = markovify.NewlineText(f.read())
 
-    text = model.make_sentence(tries=25)
+    text = None
 
-    chat = msg["chat"]["id"]
-    send_message(chat, text)
+    if matches.group(1):
+        try:
+            text = model.make_sentence_with_start(matches.group(1), strict=False, tries=25)
+        except:
+            send_message(chat, "a olha minha cabeca explsodiu fazendo essa frase tenta otra coisa....")
+    else:
+        text = model.make_sentence(tries=25)
+
+    if text is not None:
+        send_message(chat, text)
+    else:
+        send_message(chat, "vo fala nd")
