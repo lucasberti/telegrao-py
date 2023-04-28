45e6d43fcef361939270fbcecade149e00787661
Lucas Berti <berti.lucas@gmail.com>
2023-04-28 17:41:52 -0300

    fix: using POST to send large messages

--- api.py
+++ api.py
@@ -2,9 +2,11 @@ import json
 import os
 import requests
 
+BASE_URL = "https://api.telegram.org/" + os.environ['REBORNKEY']
+
 def get_updates(offset=0, timeout=60):
     """ Por default, faz longpoll. Retorna uma array de Update """
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/getUpdates?"
+    url = BASE_URL + "/getUpdates?"
     url += "offset=" + str(offset) + "&"
     url += "timeout=" + str(timeout) + "&"
 
@@ -25,19 +27,22 @@ def get_updates(offset=0, timeout=60):
 def send_message(chat_id, text, parse_mode="", reply_to_message_id="", reply_markup="", disable_web_page_preview="false"):
     """ reply_markup não é apenas ID, é uma array com opções. """
  
-    #text = text.replace("c", "k").replace("qu", "k")
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendMessage?"
-    url += "chat_id=" + str(chat_id) + "&"
-    url += "text=" + text + "&"
-    url += "disable_web_page_preview=" + disable_web_page_preview + "&" 
-    url += "parse_mode=" + parse_mode + "&"
-    if reply_to_message_id:
-        url += "reply_to_message_id=" + str(reply_to_message_id) + "&"
-    if reply_markup:
-        url += "reply_markup=" + str(reply_markup)
+    response = None
+    url = BASE_URL + "/sendMessage?"
 
-    response = requests.get(url)
-    response = json.loads(response.content)
+    params = {
+        "chat_id": chat_id,
+        "text": text,
+        "disable_web_page_preview": disable_web_page_preview,
+        "parse_mode": parse_mode
+    }
+
+    if reply_to_message_id:
+        params["reply_to_message_id"] = str(reply_to_message_id)
+    if reply_markup:
+        params["reply_markup"] = str(reply_markup)
+
+    response = requests.post(url, params=params).json()
 
     print(response)
 
@@ -46,7 +51,7 @@ def send_message(chat_id, text, parse_mode="", reply_to_message_id="", reply_mar
 
 def edit_message_text(chat_id, msg_id, text, parse_mode="Markdown", reply_to_message_id="", reply_markup=""):
     """ reply_markup não é apenas ID, é uma array com opções. """
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/editMessageText?"
+    url = BASE_URL + "/editMessageText?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "message_id=" + str(msg_id) + "&"
     url += "text=" + text + "&"
@@ -63,7 +68,7 @@ def edit_message_text(chat_id, msg_id, text, parse_mode="Markdown", reply_to_mes
 
 
 def delete_message(chat_id, msg_id):
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/deleteMessage?"
+    url = BASE_URL + "/deleteMessage?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "message_id=" + str(msg_id)
 
@@ -75,7 +80,7 @@ def delete_message(chat_id, msg_id):
 
 def send_photo(chat_id, photo_url, caption="", reply_to_message_id=0):
     """ reply_markup não é apenas ID, é uma array com opções. """
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendPhoto?"
+    url = BASE_URL + "/sendPhoto?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "photo=" + photo_url + "&"
 
@@ -93,7 +98,7 @@ def send_photo(chat_id, photo_url, caption="", reply_to_message_id=0):
 
 def send_video(chat_id, video_url, caption="", reply_to_message_id=0):
     """ reply_markup não é apenas ID, é uma array com opções. """
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendVideo?"
+    url = BASE_URL + "/sendVideo?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "video=" + video_url + "&"
 
@@ -111,7 +116,7 @@ def send_video(chat_id, video_url, caption="", reply_to_message_id=0):
 
 def send_document(chat_id, document_url, caption="", reply_to_message_id=0):
     """ reply_markup não é apenas ID, é uma array com opções. """
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendDocument?"
+    url = BASE_URL + "/sendDocument?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "document=" + document_url + "&"
 
@@ -127,7 +132,7 @@ def send_document(chat_id, document_url, caption="", reply_to_message_id=0):
 
 
 def send_sticker(chat_id, sticker_id): 
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendSticker?"
+    url = BASE_URL + "/sendSticker?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "sticker=" + sticker_id + "&"
 
@@ -138,7 +143,7 @@ def send_sticker(chat_id, sticker_id):
 
 
 def send_voice(chat_id, voice_id): 
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendvoice?"
+    url = BASE_URL + "/sendvoice?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "voice=" + voice_id + "&"
 
@@ -149,7 +154,7 @@ def send_voice(chat_id, voice_id):
 
 
 def send_chat_action(chat_id, action):
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendChatAction?"
+    url = BASE_URL + "/sendChatAction?"
     url += "chat_id=" + str(chat_id) + "&"
     url += "action=" + action + "&"
 
@@ -160,7 +165,7 @@ def send_chat_action(chat_id, action):
 
 
 def send_media_group(chat_id, files, reply_to_message_id=0):
-    url = "https://api.telegram.org/" + os.environ['REBORNKEY'] + "/sendMediaGroup?"
+    url = BASE_URL + "/sendMediaGroup?"
     url += "chat_id=" + str(chat_id) + "&"
 
     if reply_to_message_id:
